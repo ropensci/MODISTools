2c63cf5606bb7c3f58f4b476e19c43771a2c190f
khufkens <koen.hufkens@gmail.com>
2018-05-30 21:38:24 +0200

    new band err test

--- tests/testthat/test_main_functions.r
+++ tests/testthat/test_main_functions.r
@@ -7,10 +7,12 @@ test_that("test download functions",{
 
   # test listing all products
   bands <- try(list_bands(product = "MOD11A2"))
+  bands_err <- try(list_bands(product = "MOD11A6"))
 
   # see if any of the runs failed
   check = !inherits(products, "try-error") &
-    !inherits(bands, "try-error")
+          !inherits(bands, "try-error") &
+          inherits(bands_err, "try-error")
 
   # check if no error occured
   expect_true(check)
